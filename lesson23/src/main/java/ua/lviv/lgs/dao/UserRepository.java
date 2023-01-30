c1ab98e8943dcde4f92b0ae666eda3c4b13d9ac8
Yaraowl <103956364+Yaraowl@users.noreply.github.com>
2023-01-30 09:56:47 +0000

    Add files via upload

---
+++ lesson23/src/main/java/ua/lviv/lgs/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package ua.lviv.lgs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.lviv.lgs.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ lesson23/src/main/java/ua/lviv/lgs/domain/User.java
@@ -0,0 +1,131 @@
+package ua.lviv.lgs.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    @Column(name = "password")
+    private String password;
+    @Lob
+    private String encodedImage;
+
+    public User() {
+    }
+    public User(User user) {
+        this.id = user.id;
+        this.email = user.email;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.role = user.role;
+        this.password = user.password;
+    }
+    public User(String email, String firstname, String lastname, UserRole role, String password) {
+        this.email = email;
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.role = role;
+        this.password = password;
+    }
+
+    public User(Integer id, String email, String firstname, String lastname, UserRole role, String password) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.role = role;
+        this.password = password;
+    }
+
+    public String getEncodedImage() {
+        return encodedImage;
+    }
+
+    public void setEncodedImage(String encodedImage) {
+        this.encodedImage = encodedImage;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && role == user.role && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, firstName, lastName, role, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstname='" + firstName + '\'' +
+                ", lastname='" + lastName + '\'' +
+                ", role=" + role +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
